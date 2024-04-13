570e8e4b3e2ab77277c1a03a34254665761a9e5f
maksiiik <maksin8008@yandex.ru>
2024-04-13 17:23:13 +0300

    dsd

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -14,26 +14,19 @@ const transporter = nodemailer.createTransport({
 router.post('/send-email', async (req, res) => {
   try {
     const { number, name, email } = req.body; 
-
-    // Валидация входных данных
     if (!number || !name || !email) {
       return res.status(400).json({ message: 'Пожалуйста, заполните все поля' });
     }
-
     const mailOptions = {
       from: 'ApecInfo@yandex.ru', 
       to: 'ApecInfo@yandex.ru',
       subject: `Имя: ${name}!\n Ваш номер: ${number}\n Почта: ${email}.`, // Тема письма
       text: `Имя: ${name}!\n Ваш номер: ${number}\n Почта: ${email}.`, 
     };
-
     await transporter.sendMail(mailOptions); 
-
-    // Обработка успешной отправки письма
     res.sendStatus(200); 
   } catch (error) {
     console.error(error);
-    // Обработка ошибки при отправке письма
     res.status(500).json({ message: 'Ошибка отправки электронного письма' });
   }
 });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import morgan from 'morgan';
+
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
@@ -11,18 +11,17 @@ import resLocals from './middlewares/resLocals';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.engine('jsx', jsxRender);
-app.set('view engine', 'jsx');
+app.engine('js', jsxRender);
+app.set('view engine', 'js');
 app.set('views', path.join(__dirname, 'components', 'pages'));
 
 app.use(express.static('public'));
-app.use(morgan('dev'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(resLocals);
 
-app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 app.listen(PORT, () => console.log(`App has started on port ${PORT}`));
